7e55f0f58b54cfc999547083fda2b06ba02ffeae
Norbert Kulacin <norbert.kulacin@gmail.com>
2023-05-12 15:43:34 +0200

    second version - react query has been implemented

--- src/context.jsx
+++ src/context.jsx
@@ -3,19 +3,31 @@ import { createContext, useContext, useState, useEffect } from "react";
 
 const AppContext = createContext();
 
+const getInitalDarkMode = () => {
+  const prefersDarkMode = window.matchMedia('(prefers-color-scheme:dark)').matches;
+  const storedDarkMode = localStorage.getItem("darkTheme") === 'true';
+  return storedDarkMode || prefersDarkMode;
+}
+
 export const AppProvider = ({ children}) => {
    
-    const [isDarkTheme, setIsDarkTheme] = useState(false);
-
+    const [isDarkTheme, setIsDarkTheme] = useState(getInitalDarkMode());
+    const [searchTerm, setSearchTerm] = useState('dog');
+  
     const toggleDarkTheme = () => {
         const newDarkTheme = !isDarkTheme
         setIsDarkTheme(newDarkTheme)
-        const body = document.querySelector('body');
-        body.classList.toggle('dark-theme', newDarkTheme);
-    }
+        localStorage.setItem('darkTheme', newDarkTheme)
+  }
+  
+  useEffect(() => {
+    document.body.classList.toggle("dark-theme", isDarkTheme);
+  },[isDarkTheme])
     
     return (
-      <AppContext.Provider value={{ isDarkTheme, toggleDarkTheme }}>
+      <AppContext.Provider
+        value={{ isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm }}
+      >
         {children}
       </AppContext.Provider>
     );
